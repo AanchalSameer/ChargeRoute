3a77243296667a6d601f2a4fdcc09726e2aec99e
AanchalSameer <147327502+AanchalSameer@users.noreply.github.com>
2025-01-15 22:50:34 +0530

    Delete CODE directory

--- CODE/server/routes/chargeStation.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getChargingStations } = require('../controllers/chargeStationController');
-
-router.get('/', getChargingStations);
-
-module.exports = router;
